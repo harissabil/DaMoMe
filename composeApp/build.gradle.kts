dcb65fcdcb2a3b9359a127f303551da6c3dfb921
Haris <harissabil04@gmail.com>
2025-01-12 13:06:03 +0700

    Update build.gradle.kts android versionCode

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -177,7 +177,7 @@ android {
         applicationId = "com.harissabil.damome"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0-beta1"
     }
     packaging {
